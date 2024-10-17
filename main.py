adbe23b301463dd22db54a8560cdb2db3347d400
SandsofTime1 <sandst2493@my.uwstout.edu>
2024-10-17 16:00:35 -0500

    API

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import requests
 
-def load_api_key(file_path):
-    with open('api-key.txt', 'r') as file:
-        return file.read().strip() #Read and get rid of whitespace
+#def load_api_key(file_path):
+#    with open('api-key.txt', 'r') as file:
+#        return file.read().strip() #Read and get rid of whitespace
 
 def get_restaurants(location):
-    api_key = load_api_key('api-key.txt')
-    #api_key ='AIzaSyA4witeIUyn2Zu9cnEodZw698Y7qcgUquk'
+    #api_key = load_api_key('api-key.txt')
+    api_key ='AIzaSyA4witeIUyn2Zu9cnEodZw698Y7qcgUquk'
     query = f"restaurants in {location}"  # Modify query to focus on restaurants
     url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&key={api_key}"
     
